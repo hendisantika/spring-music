b761a46ba920d6c747936094c9e957272ad2c812
Hendi Santika <hendisantika@gmail.com>
2017-05-03 06:57:24 +0700

    #1 - Spring Music

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -3,10 +3,10 @@ package org.cloudfoundry.samples.music;
 import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+//public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).
