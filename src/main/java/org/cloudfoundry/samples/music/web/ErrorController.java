89835e6740b53b741a03b7844dff4559c07b2572
hendisantika <hendisantika@yahoo.co.id>
2025-07-06 06:29:21 +0700

    fix: ErrorController & InfoController🫸🌀✏️📗🐧🐳

--- src/main/java/org/cloudfoundry/samples/music/web/ErrorController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/ErrorController.java
@@ -2,6 +2,7 @@ package org.cloudfoundry.samples.music.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ErrorController {
     private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
-    @RequestMapping(value = "/kill")
+    @GetMapping("/kill")
     public void kill() {
         logger.info("Forcing application exit");
         System.exit(1);
     }
 
-    @RequestMapping(value = "/throw")
+    @GetMapping("/throw")
     public void throwException() {
         logger.info("Forcing an exception to be thrown");
         throw new NullPointerException("Forcing an exception to be thrown");
--- src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
@@ -2,52 +2,36 @@ package org.cloudfoundry.samples.music.web;
 
 import org.cloudfoundry.samples.music.domain.ApplicationInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.service.ServiceInfo;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class InfoController {
-    @Autowired(required = false)
-    private Cloud cloud;
-
-    private Environment springEnvironment;
+    private final Environment springEnvironment;
 
     @Autowired
     public InfoController(Environment springEnvironment) {
         this.springEnvironment = springEnvironment;
     }
 
-    @RequestMapping(value = "/appinfo")
+    @GetMapping("/appinfo")
     public ApplicationInfo info() {
         return new ApplicationInfo(springEnvironment.getActiveProfiles(), getServiceNames());
     }
 
-    @RequestMapping(value = "/service")
-    public List<ServiceInfo> showServiceInfo() {
-        if (cloud != null) {
-            return cloud.getServiceInfos();
-        } else {
-            return new ArrayList<>();
-        }
+    @GetMapping("/service")
+    public List<Map<String, Object>> showServiceInfo() {
+        // Cloud services information is not available without Spring Cloud Connectors
+        return new ArrayList<>();
     }
 
     private String[] getServiceNames() {
-        if (cloud != null) {
-            final List<ServiceInfo> serviceInfos = cloud.getServiceInfos();
-
-            List<String> names = new ArrayList<>();
-            for (ServiceInfo serviceInfo : serviceInfos) {
-                names.add(serviceInfo.getId());
-            }
-            return names.toArray(new String[names.size()]);
-        } else {
-            return new String[]{};
-        }
+        // Cloud services information is not available without Spring Cloud Connectors
+        return new String[]{};
     }
 }
