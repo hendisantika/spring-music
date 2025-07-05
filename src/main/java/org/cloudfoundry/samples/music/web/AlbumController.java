481e9d259047485bdeccb5a9b5c8fb204ee7bed3
hendisantika <hendisantika@yahoo.co.id>
2025-07-06 06:28:37 +0700

    fix: Album Controller🫸🌀✏️📗🐧🐳

--- src/main/java/org/cloudfoundry/samples/music/web/AlbumController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/AlbumController.java
@@ -1,51 +1,57 @@
 package org.cloudfoundry.samples.music.web;
 
+import jakarta.validation.Valid;
 import org.cloudfoundry.samples.music.domain.Album;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/albums")
 public class AlbumController {
     private static final Logger logger = LoggerFactory.getLogger(AlbumController.class);
-    private CrudRepository<Album, String> repository;
+    private final CrudRepository<Album, String> repository;
 
     @Autowired
     public AlbumController(CrudRepository<Album, String> repository) {
         this.repository = repository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public Iterable<Album> albums() {
         return repository.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public Album add(@RequestBody @Valid Album album) {
         logger.info("Adding album " + album.getId());
         return repository.save(album);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public Album update(@RequestBody @Valid Album album) {
         logger.info("Updating album " + album.getId());
         return repository.save(album);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public Album getById(@PathVariable String id) {
         logger.info("Getting album " + id);
-        return repository.findOne(id);
+        return repository.findById(id).orElse(null);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable String id) {
         logger.info("Deleting album " + id);
-        repository.delete(id);
+        repository.deleteById(id);
     }
 }
